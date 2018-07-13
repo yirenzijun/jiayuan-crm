d2fe1a13675af62f9457005d631b316f332a7fdd
yirenzijun <lywork01@126.com>
2018-07-13 11:43:27 +0800

    git提交

---
+++ base-views-spring-cloud/src/main/java/com/jiayuan/crm/baseviewsspringcloud/BaseViewsSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package com.jiayuan.crm.baseviewsspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseViewsSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseViewsSpringCloudApplication.class, args);
+    }
+}
